c1aee699cb36c7d7a90592312e18d15234d64a15
LYAM <cous5830@gmail.com>
2025-08-03 20:04:36 +0000

    Global variable template with code header done.

--- Variables/template.lua
+++ Variables/template.lua
@@ -1,3 +1,8 @@
---[[
-    @name
-]]
+-- `YY/MM/DD`
+-- ### Description
+-- What the variable is
+-- ### Used for
+-- What do you use the variable for
+-- ### Held data
+-- The structure of the data within the variable
+g_name = nil
