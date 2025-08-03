20c7f664b31c813716e71a55f045785b8a910b49
LYAM <cous5830@gmail.com>
2025-08-03 21:40:01 +0000

    Main.lua for preprocessor stuff

---
+++ Projects/preprocessor_test/main.lua
@@ -0,0 +1,27 @@
+-- [BRS] - [[ Information ]] --
+-- `YY/MM/DD`
+-- ## Generic info:
+-- #### MC : `Microcontroller ID`
+-- #### SN : `Script number
+-- ## What does this do?
+-- description
+-- ## Where is this used?
+-- Explain
+
+-- [BRS] - [[  imports  ]] --
+
+-- [BRS] - [[ variables ]] --
+
+-- [BRS] - [[   mains   ]] --
+
+function onTick()
+    -- [BRS] - [[ Inputs ]] --
+
+    -- [BRS] - [[ Outputs ]] --
+end
+
+function onDraw()
+
+end
+
+-- [BRS] - [[   Functions   ]] --
