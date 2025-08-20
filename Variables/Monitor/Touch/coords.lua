f563cf57c5b6f5871128fab09137d49cadd7b4db
LYAM <cous5830@gmail.com>
2025-08-20 10:53:59 -0400

    Global variables for monitor pressing

---
+++ Variables/Monitor/Touch/coords.lua
@@ -0,0 +1,8 @@
+-- [BRS] - [[ Information ]] --
+-- `25/08/19`
+-- ### Description
+-- The monitor pixel coordinates where the player is actively pressing.
+-- ### Used for
+-- GUI elements.
+g_pressingCoordsX = 0
+g_pressingCoordsY = 0
---
+++ Variables/Monitor/Touch/pressing.lua
@@ -0,0 +1,7 @@
+-- [BRS] - [[ Information ]] --
+-- `25/08/19`
+-- ### Description
+-- Holds `true` if the player is pressing the monitor. `false` otherwise.
+-- ### Used for
+-- GUI elements.
+g_pressing = false
