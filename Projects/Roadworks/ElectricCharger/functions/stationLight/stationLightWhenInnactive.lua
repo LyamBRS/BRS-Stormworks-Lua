d9142882999531bdd92ea9e4019c5cc75c61de2e
LYAM <cous5830@gmail.com>
2025-12-27 17:41:05 +0000

    Function managing charger station status light when station is innactive

---
+++ Projects/Roadworks/ElectricCharger/functions/stationLight/stationLightWhenInnactive.lua
@@ -0,0 +1,28 @@
+-- [BRS] - [[ Information ]] --
+-- `2025/12/27`
+-- ### Description
+-- handles the charging station's RGB status light's global variable
+-- when the station is innactive. Being innactive means not actively
+-- charging or discharging.
+-- ### Used for
+-- Sets it as green when no signal from the antenna is detected.
+-- Sets it as yellow if a signal is detected from the antenna.
+-- Sets it as red if infinite electric is detected.
+-- ### @Input
+-- None
+-- ### @Returns
+-- None
+function stationLightWhenInnactive()
+    if g_antennaSignalStrength > 0 then 
+        -- [BRS] - vehicle is connected. Don't show green. Station taken
+        g_stationLight = {1, 1, 0}
+    else 
+        -- [BRS] - there's no vehicle connected. Station is available.
+        g_stationLight = {0, 1, 0}
+    end
+
+    if g_infElectric then
+        -- [BRS] - Station is unavailable when infinite electric is on.
+        g_stationLight = {1, 0, 0}
+    end
+end
