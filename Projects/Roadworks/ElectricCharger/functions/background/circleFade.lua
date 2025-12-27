27a7b1d2323adfb3cf6c48b54db3c65461481cf0
LYAM <cous5830@gmail.com>
2025-12-27 17:35:32 +0000

    circleFade optimized and reduced by 101 minified chars

--- Projects/Roadworks/ElectricCharger/functions/background/circleFade.lua
+++ Projects/Roadworks/ElectricCharger/functions/background/circleFade.lua
@@ -1,41 +1,47 @@
 require("Functions.Animations.Framework.lerpAnimation")
+require("Variables.Animations.current")
 
 function circleFade(x,y, x2, y2, startColor, endColor, sizing, startRatio, endRatio, distanceBetweenPoints, deltaX, deltaY)
 	drawingIncrementRatio = 1/(distanceBetweenPoints/5)
 	ratioDiff = endRatio-startRatio
-	
-	startRed = startColor[1][c_animationCurrent]
-	startGreen = startColor[2][c_animationCurrent]
-	startBlue = startColor[3][c_animationCurrent]
-	startAlpha = startColor[4][c_animationCurrent]
-	
-	endRed = endColor[1][c_animationCurrent]
-	endGreen = endColor[2][c_animationCurrent]
-	endBlue = endColor[3][c_animationCurrent]
-	endAlpha = endColor[4][c_animationCurrent]
-	
+
 	-- Goes from 0% to 100% of the distance between the 2 coordinates.
 	-- Increments in % steps equivalent to a pixel.
 	-- Percentages were used to avoid the calculation of the 1/x ration every loop iteration.
 	for position=0, 1, drawingIncrementRatio do
-		circleX = deltaX * position + x
-		circleY = deltaY * position + y
+		-- circleX = deltaX * position + x
+		-- circleY = deltaY * position + y
 		globalRatio = ratioDiff * position + startRatio
 		
-		effectRatio = (-((2*globalRatio - 1)^2) + 1)
-		size = effectRatio * sizing
 		-- Color management
-		red   = lerpAnimation(startRed, endRed, globalRatio)
-		green = lerpAnimation(startGreen, endGreen, globalRatio)
-		blue  = lerpAnimation(startBlue, endBlue, globalRatio)
-		alpha = lerpAnimation(startAlpha, endAlpha, globalRatio)
-		screen.setColor(red, green, blue, alpha)
+		-- [BRS] - Equivalent of:
+		-- `red = lerpAnimation(startColor, endColor, ratio)`
+		-- `setColor(red)`
+		-- doing it this way rather than using a bunch of variables for readability, saves `60` minified characters.
+		screen.setColor(
+			lerpAnimation(startColor[1][c_animationCurrent], endColor[1][c_animationCurrent], globalRatio),
+			lerpAnimation(startColor[2][c_animationCurrent], endColor[2][c_animationCurrent], globalRatio),
+			lerpAnimation(startColor[3][c_animationCurrent], endColor[3][c_animationCurrent], globalRatio),
+			lerpAnimation(startColor[4][c_animationCurrent], endColor[4][c_animationCurrent], globalRatio)
+		)
 		-- Calculates the size of the circle based on its position on the line.
 		-- Ensures circles are bigger at the center of the line
 		-- Function shaped like an eye. Original function couldn't be used
 		-- due to computing discrepancies between Desmos and Pony IDE.
 		-- Better computation times than using sin functions.
-		effectRatio = (-((2*globalRatio - 1)^2) + 1)
-		screen.drawCircleF(circleX, circleY, effectRatio * sizing)
+		-- [BRS] - [[Minifications]]:
+		-- Used to be:
+		-- - `circleX = deltaX * position + x`
+		-- - `circleY = deltaY * position + y`
+		-- - `effectRatio = (-((2*globalRatio - 1)^2) + 1)`
+		-- - `screen.drawCircleF(circleX, circleY, effectRatio * sizing)`
+		-- Doing it all in the function call does:
+		-- - Saves `12` minified characters
+		-- - Brings fps from `44~` to `50~`
+		screen.drawCircleF(
+			deltaX * position + x,
+			deltaY * position + y,
+			(-((2*globalRatio - 1)^2) + 1) * sizing	-- Ensures the width of the curve is thicker at the middle
+		)
 	end
 end
