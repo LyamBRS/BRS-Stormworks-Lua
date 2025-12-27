385af93d718353737e8e5d43960df4f879722992
LYAM <cous5830@gmail.com>
2025-12-27 03:07:32 +0000

    testing BRS logo for chargers

--- Projects/Roadworks/ElectricCharger/main.lua
+++ Projects/Roadworks/ElectricCharger/main.lua
@@ -1,5 +1,8 @@
 require("Projects.Roadworks.ElectricCharger.constants.constants")
 require("Projects.Roadworks.ElectricCharger.functions.functions")
+require("Functions.Drawing.Logo.brs_b")
+require("Functions.Drawing.Logo.brs_r")
+require("Functions.Drawing.Logo.brs_s")
 
 function onTick()
     -- [BRS] - Empty for now.
@@ -7,6 +10,11 @@ end
 
 function onDraw()
     require("Projects.Roadworks.ElectricCharger.onDraw.drawBackground")
+
+    screen.setColor(255,255,255)
+    logoB(0, 0, 1)
+    logoR(6, 0, 1)
+    logoS(12, 0, 1)
 end
 
 require("Projects.Roadworks.ElectricCharger.beforeScript.beforeScript")
