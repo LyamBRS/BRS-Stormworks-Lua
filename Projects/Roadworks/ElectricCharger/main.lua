3cd4fcb05754253aecc93c6ec1d7e9212a707318
LYAM <cous5830@gmail.com>
2025-12-27 05:46:46 +0000

    added temporary logo drawing tests

--- Projects/Roadworks/ElectricCharger/main.lua
+++ Projects/Roadworks/ElectricCharger/main.lua
@@ -1,20 +1,52 @@
 require("Projects.Roadworks.ElectricCharger.constants.constants")
 require("Projects.Roadworks.ElectricCharger.functions.functions")
-require("Functions.Drawing.Logo.brs_b")
-require("Functions.Drawing.Logo.brs_r")
-require("Functions.Drawing.Logo.brs_s")
 
+state = 0
 function onTick()
-    -- [BRS] - Empty for now.
+    -- [BRS] - Temporary test of boot up logo
+    if state == 2 then
+        animate(brsLogo[1][c_yIndex])
+        animate(brsLogo[2][c_yIndex])
+        animate(brsLogo[3][c_yIndex])
+
+        if brsLogo[3][c_yIndex][c_animationTimeLeft] == 0 then
+            state = 3
+        end
+    end
+
+    if state == 1 then -- Bring the logo to the middle of the screen
+        animate(brsLogo[4])                     -- animate the sweeping
+        animate(brsLogo[1][c_yIndex])           -- The B can already come downwards.
+        sweep = brsLogo[4][c_animationCurrent]
+
+        if sweep > 1 then
+            animate(brsLogo[2][c_yIndex])
+        end
+
+        if sweep > 2 then
+            animate(brsLogo[3][c_yIndex])
+        end
+        
+        if brsLogo[3][c_yIndex][c_animationTimeLeft] == 0 then -- the S 
+            setNewAnimationTarget(brsLogo[1][c_yIndex], 75, 50)
+            setNewAnimationTarget(brsLogo[2][c_yIndex], 75, 50)
+            setNewAnimationTarget(brsLogo[3][c_yIndex], 75, 50)
+            state = 2
+        end
+    end
+
+    if state == 0 then -- Set the animations up
+        setNewAnimationTarget(brsLogo[1][c_yIndex], 20, 50)
+        setNewAnimationTarget(brsLogo[2][c_yIndex], 20, 50)
+        setNewAnimationTarget(brsLogo[3][c_yIndex], 20, 50)
+        setNewAnimationTarget(brsLogo[4], 3, 30)
+        state = 1
+    end
 end
 
 function onDraw()
     require("Projects.Roadworks.ElectricCharger.onDraw.drawBackground")
-
-    screen.setColor(255,255,255)
-    logoB(0, 0, 1)
-    logoR(6, 0, 1)
-    logoS(12, 0, 1)
+    require("Projects.Roadworks.ElectricCharger.onDraw.drawingLogo")
 end
 
 require("Projects.Roadworks.ElectricCharger.beforeScript.beforeScript")
