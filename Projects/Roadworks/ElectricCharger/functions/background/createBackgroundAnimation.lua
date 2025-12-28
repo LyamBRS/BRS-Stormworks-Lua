c5ba56ae38ef4e5c7ec50e5097cf4f6a1ee2252d
LYAM <cous5830@gmail.com>
2025-12-28 18:08:22 +0000

    18 character saved in createBackground

--- Projects/Roadworks/ElectricCharger/functions/background/createBackgroundAnimation.lua
+++ Projects/Roadworks/ElectricCharger/functions/background/createBackgroundAnimation.lua
@@ -1,18 +1,16 @@
-require("Projects.Roadworks.ElectricCharger.functions.background.handleAnimationEnd")
 require("Functions.Animations.Framework.quintInOutAnimation")
 require("Functions.Animations.Framework.createAnimation")
-require("Variables.Animations.target")
-require("Variables.Animations.current")
-require("Variables.Animations.timeLeft")
 -- [BRS] - [[Information]]
 -- The goal is to create a new animation object
 -- Using random ranges
 -- And make it immediately reach the target
 -- ensures the screen doesn't start all dark and at 0.
 function createBackgroundAnimation(min, max)
-    animation = createAnimation(0, 0, 0, quintInOutAnimation)
-    handleAnimationEnd(animation, min, max)
-    animation[c_animationCurrent] = animation[c_animationTarget]
-    animation[c_animationTimeLeft] = 0
-    return animation
+    start = math.random(min, max)
+    return createAnimation(
+        start,
+        start,
+        0,
+        quintInOutAnimation
+    )
 end
