a5af299db57efb5d107bfabeab4dbcfd858319b6
LYAM <cous5830@gmail.com>
2025-12-29 19:12:46 +0000

    Fixed indentations

--- Projects/Roadworks/ElectricCharger/onDraw/drawingLogo.lua
+++ Projects/Roadworks/ElectricCharger/onDraw/drawingLogo.lua
@@ -1,4 +1,4 @@
-screen.setColor(128,0,255,128)
-logoB(brsLogoBX, brsLogoBY[c_animationCurrent], 4)
-logoR(brsLogoRX, brsLogoRY[c_animationCurrent], 4)
-logoS(brsLogoSX, brsLogoSY[c_animationCurrent], 4)
+    screen.setColor(128,0,255,128)
+    logoB(brsLogoBX, brsLogoBY[c_animationCurrent], 4)
+    logoR(brsLogoRX, brsLogoRY[c_animationCurrent], 4)
+    logoS(brsLogoSX, brsLogoSY[c_animationCurrent], 4)
