3390873a3a7af1288d864fbd4bd954cb5957cf48
LYAM <cous5830@gmail.com>
2025-12-29 19:29:10 +0000

    Fixed discharging message being off center

--- Projects/Roadworks/ElectricCharger/onDraw/drawChargingVehicle.lua
+++ Projects/Roadworks/ElectricCharger/onDraw/drawChargingVehicle.lua
@@ -2,7 +2,7 @@
     g_chargingVehicleText[c_elementFunction](g_chargingVehicleText)
 
     screen.setColor(64,32,16,240)
-    suffix = g_wantsToCharge and "charging" or "discharging"
+    suffix = g_wantsToCharge and "charging" or "    discharging  "
     drawTextOnSurface(g_chargingVehicleTextSurface, suffix.." vehicle")
 
     screen.setColor(32,32,32,240)
