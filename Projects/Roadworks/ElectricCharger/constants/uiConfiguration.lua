90c709be56cbb968ba57e28b77dd80e543654d59
LYAM <cous5830@gmail.com>
2025-12-29 19:28:10 +0000

    UI sweep speeds now a constant variable

--- Projects/Roadworks/ElectricCharger/constants/uiConfiguration.lua
+++ Projects/Roadworks/ElectricCharger/constants/uiConfiguration.lua
@@ -1,2 +1,5 @@
 -- [BRS] - An important action is going on. The player left... wait this long (in ticks) until booting off.
 c_timeToWaitAfterPlayer = 20
+
+-- [BRS] - Tells the amount of ticks UI shifting animations should last for, when we move from one page to another.
+c_UIShiftDuration = 80
