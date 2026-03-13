dbc9ff856636b02f4d365bd3c699d38b0adfec24
LYAM <cous5830@gmail.com>
2026-03-13 21:02:42 +0000

    base64 image decoder from legacy code

---
+++ Functions/Base64/Images/decodeColoredImage.lua
@@ -0,0 +1,59 @@
+require("Functions.Strings.split")
+require("Functions.Base64.Conversions.letterToNumber")
+-- [BRS] - [[ Information ]] --
+-- `26/03/13`
+-- ### Description
+-- Decodes colored base64 string encoded images, into drawable images.
+-- This is the full function. Ensure your decoder functions ALWAYS WORK FOR THESE IMAGES.
+--
+-- ### Used for
+-- Drawing complicated icons without saving all their coordinates directly in the code.
+-- Or receiving icons from BRIO, then displaying them.
+-- You utilize this only if the image has colors AND is only for specific dimensions.
+-- ### @Input
+-- ##### - `encoded` : `string`
+-- Encoded image that follows the following encoding:
+-- 
+-- `Color definitions @ color index: rectangle dimensions ? color index: rectangle dimensions`
+--
+-- `RGB1 RGB2 RGB3 @ 1 : x,y,w,h x,y,w,h ? 2: x,y,w,h`
+--
+-- `RGBRGBRGB@1:XYWHXYWH?2:XYWH`
+-- ### @Returns
+-- ##### 1. `image` : `table`
+-- {{{r,g,b},{{x,y,w,h}, {x,y,w,h}}}, {{r,g,b}, {{x,y,w,h}}}}
+function decodeImage(encoded)
+    -- [BRS] - Seperates the color definitions from their rectangle usages
+    colorsSplit = split(encoded, "@")
+
+    -- [BRS] - Color parsing. If you're using BRS Themes, you could reduce character usage by making this portion a function.
+    colors = {}
+    for i=1, #colorsSplit[1], 3 do
+        color = colorsSplit[1]:sub(i, i+2)
+        temporary = {}
+        for j=1, 3 do
+            temporary[j] = ((numberFromBase64(color:sub(j,j))-1)/63)*255
+        end
+        colors[#colors+1] = temporary
+    end
+
+    -- [BRS] - Creating the bundles of rectangles based on the color they use.
+    image = {}
+    -- [BRS] - Bundle separation, color:xywhxywhxywh
+	for _,v in pairs(split(colorsSplit[2],"?")) do
+		i = #image+1
+		bundle = split(v,":")
+		image[i] = {colors[numberFromBase64(bundle[1])],{}}
+
+        -- [BRS] - For every groups of rectangle positions (thus the increase of 4)
+		for j=1,#bundle[2],4 do
+			temporary = {}
+			rectangle = bundle[2]:sub(j,j+3)
+			for c=1,4 do
+				temporary[c] = numberFromBase64(rectangle:sub(c,c))-1
+			end
+			image[i][2][j] = temporary
+		end
+    end
+    return image
+end
