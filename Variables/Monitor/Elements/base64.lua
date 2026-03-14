ad2d59d058a9c16b8716cd3b61910b5e473cf5c3
LYAM <cous5830@gmail.com>
2026-03-14 21:24:48 +0000

    Element framework addition for base64

---
+++ Variables/Monitor/Elements/base64.lua
@@ -0,0 +1,10 @@
+-- [BRS] - [[ Information ]] --
+-- `2026/03/14`
+-- ### Description
+-- Index of the element object table, where the element's 
+-- base64 encoded image is found.
+-- ### Used for
+-- drawing animated base64 images or complex buttons
+-- ### Held data
+-- constant table index; `2`.
+c_elementBase64 = 5
