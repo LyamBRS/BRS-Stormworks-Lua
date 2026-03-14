d3c6dab81fd355ee4ff428ba8661f383786932c8
LYAM <cous5830@gmail.com>
2026-03-14 21:30:21 +0000

    Fixed the gradiant!!!

--- Projects/Microprocessors/Dashboard/onDraw/background.lua
+++ Projects/Microprocessors/Dashboard/onDraw/background.lua
@@ -1,12 +1,9 @@
 -- [BRS] - Draws the main gradiant background of the dashboard.
+screen.setColor(25,25,25)
+screen.drawClear()
+
 horizontalGradiant(
     0,0,96,32,
     62,54,41,255,
     62,54,41,0
-)
-
-horizontalGradiant(
-    0,0,96,32,
-    25,25,25,0,
-    25,25,25,255
 )
