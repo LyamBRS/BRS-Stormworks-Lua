55fe6f1e882873db37eaf8e567aabe233889fda8
LYAM <cous5830@gmail.com>
2026-03-15 23:34:59 +0000

    Dashboard manager now outputs its state

--- Projects/Microprocessors/Dashboard/manager/constants/outputChannels.lua
+++ Projects/Microprocessors/Dashboard/manager/constants/outputChannels.lua
@@ -4,3 +4,4 @@ c_sbPrebootedOn = 3 -- Indicates if the preboot screen should be on or not.
 c_sbBootedOn = 4    -- Indicates if the booted screen should be on or not.
 
 -- [BRS] ------------------------------------------------ [[ NUMERICAL ]] -
+c_snCurrentState = 1 -- Indicates to scripts like the background manager, where we are in the boot process.
--- Projects/Microprocessors/Dashboard/manager/onTick/setOutputs.lua
+++ Projects/Microprocessors/Dashboard/manager/onTick/setOutputs.lua
@@ -2,3 +2,6 @@
 output.setBool(c_sbPrebootedOn, g_showPreBoot)
 output.setBool(c_sbAllActive, g_scriptsOn)
 output.setBool(c_sbBootedOn, g_showBooted)
+
+-- [BRS] ------------------------------------------------ [[ NUMERICAL ]] -
+output.setNumber(c_snCurrentState, g_state)
