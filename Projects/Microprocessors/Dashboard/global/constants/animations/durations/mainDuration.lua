3611f4262433f493cf1397ef6078bb6cfd73a6b2
LYAM <cous5830@gmail.com>
2026-03-15 23:33:19 +0000

    Global constants for dashboard animations

---
+++ Projects/Microprocessors/Dashboard/global/constants/animations/durations/mainDuration.lua
@@ -0,0 +1,8 @@
+-- [BRS] - [[ Information ]] -
+-- This contains the transition durations for a bunch of stuff.
+-- Evaluated in ticks, NEVER EVER have floating points in here, you'll break the program.
+c_animationDurationLongest = 48
+c_animationDurationLong = 32
+c_animationDurationNormal = 24
+c_animationDurationShort = 16
+c_animationDurationShortest = 8
