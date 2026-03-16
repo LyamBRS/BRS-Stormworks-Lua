0d53d0aa08f6598474399f9a41e8bd6cbb5d1366
LYAM <cous5830@gmail.com>
2026-03-16 22:36:49 +0000

    Dashboard global constats for font sizes and monitor sizes

---
+++ Projects/Microprocessors/Dashboard/global/constants/positions/brsFont.lua
@@ -0,0 +1,6 @@
+-- [BRS] - Dimensions of the font used in the dashboard.
+-- Used for vehicle names for example.
+c_companyFontWidth = 5
+c_companyFontHeight = 5
+c_companyFontSpacing = 1
+c_companyFontFullWidth = 6 -- Builder unable to compute this yet: c_companyFontWidth * c_companyFontSpacing
---
+++ Projects/Microprocessors/Dashboard/global/constants/positions/monitor.lua
@@ -0,0 +1,4 @@
+-- [BRS] - Size of the monitor
+-- Why bother with wasted characters when you can hardcode it here and that it'll always be a 3x1.
+c_monitorWidth = 96
+c_monitorHeight = 32
