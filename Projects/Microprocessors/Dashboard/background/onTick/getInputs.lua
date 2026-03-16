7585c7e1bc4b0baffc2818a83c8fd9ec321b88ff
LYAM <cous5830@gmail.com>
2026-03-16 22:36:30 +0000

    removal of unused getters in background manager

--- Projects/Microprocessors/Dashboard/background/onTick/getInputs.lua
+++ Projects/Microprocessors/Dashboard/background/onTick/getInputs.lua
@@ -1,6 +1,6 @@
 -- [BRS] ------------------------------------------------ [[ BOOLEAN ]] -
-g_vehicleOn     = input.getBool(c_gbVehicleOn)
-g_playerSensor  = input.getBool(c_gbPlayerSensor)
+-- g_vehicleOn     = input.getBool(c_gbVehicleOn)
+-- g_playerSensor  = input.getBool(c_gbPlayerSensor)
 g_scriptsOn     = input.getBool(c_gbScriptActive)
 
 -- [BRS] ------------------------------------------------ [[ NUMERICAL ]] -
