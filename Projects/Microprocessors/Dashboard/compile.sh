df70b79ce540ddc6f7a7a6bee4e543a71bea563e
LYAM <cous5830@gmail.com>
2026-03-16 22:35:06 +0000

    Compiler now has preboot script

--- Projects/Microprocessors/Dashboard/compile.sh
+++ Projects/Microprocessors/Dashboard/compile.sh
@@ -5,6 +5,7 @@ GENERATED_DIR="$PROJECT_ROOT/generated"
 
 BACKGROUND_OUTPUT="$GENERATED_DIR/background.lua"
 MANAGER_OUTPUT="$GENERATED_DIR/manager.lua"
+PREBOOT_OUTPUT="$GENERATED_DIR/preboot.lua"
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "Script directory: $SCRIPT_DIR"
@@ -19,3 +20,4 @@ touch "$MANAGER_OUTPUT"
 
 preprocessor "./$PROJECT_ROOT/background/main.lua" --output "$BACKGROUND_OUTPUT"
 preprocessor "./$PROJECT_ROOT/manager/main.lua" --output "$MANAGER_OUTPUT"
+preprocessor "./$PROJECT_ROOT/preboot/main.lua" --output "$PREBOOT_OUTPUT"
