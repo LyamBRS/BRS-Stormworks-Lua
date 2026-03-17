f642bd71b7a13a268442e4d1da4cdcb7c4b1365e
LYAM <cous5830@gmail.com>
2026-03-17 00:00:57 +0000

    Binding comment

--- Projects/Microprocessors/Dashboard/manager/constants/inputsChannels.lua
+++ Projects/Microprocessors/Dashboard/manager/constants/inputsChannels.lua
@@ -2,7 +2,7 @@
 c_gbVehicleOn = 7       -- Vehicle is on and operational
 c_gbPlayerSensor = 30   -- A player is nearby
 
-c_gbPrebootedOn = 1     -- The preboot UI script is ON
+c_gbPrebootedOn = 1     -- The preboot UI script is ON. Preboot uses c_sbUIShown
 c_gbBootedOn = 2        -- The main UI script is ON
 
 -- [BRS] ------------------------------------------------ [[ NUMERICAL ]] -
