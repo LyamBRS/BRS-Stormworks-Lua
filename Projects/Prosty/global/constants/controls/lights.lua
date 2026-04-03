07962641e392d347ccf955b80b8bd3d28650bd8b
LYAM <cous5830@gmail.com>
2026-04-03 20:15:01 +0000

    Lights setting externalized to a setting constant file. Plus done

--- Projects/Prosty/global/constants/controls/lights.lua
+++ Projects/Prosty/global/constants/controls/lights.lua
@@ -7,11 +7,3 @@ c_lightSettingDRL = 1           -- Only DRL
 c_lightSettingAutomatic = 2     -- Lights are handled automatically
 c_lightSettingLowBeams = 3      -- Low beams
 c_lightSettingHighBeams = 4     -- High beams
-
--- [BRS] - Automatic light settings
-c_distanceTunnelDetection = 5.5     -- Distance the top distance sensor needs to read to turn on the low beams. Keep low to avoid hangar.
-c_clockNightTime          = 0.68    -- Time where the low beams should turn on
-c_clockDayTime            = 0.31    -- Time where the low beams should turn off
-
-c_lightAutoHighDetectionFOV = 0.08    -- Compared to the front of the car, Whats the lowest angle difference allowed in which there must be no vehicle for automatic high beams to turn on?
-c_lightAutoHighDetectionDuration = 1  -- Time in ticks before high beams turn on after not detecting a car anymore.
---
+++ Projects/Prosty/global/constants/settings/lights.lua
@@ -0,0 +1,16 @@
+
+-- [BRS] - Automatic light settings
+c_distanceTunnelDetection = 7.5    -- Distance the top distance sensor needs to read to turn on the low beams. Keep low to avoid hangar.
+c_clockNightTime          = .67    -- Time where the low beams should turn on
+c_clockNightTimeHigh      = .71    -- Time where the high beams should be allowed on.
+c_clockDayTimeHigh        = .272   -- Time where the high beams should turn off
+c_clockDayTime            = .32    -- Time where the low beams should turn off
+
+c_lightAutoHighDetectionFOV      = .15 -- Compared to the front of the car, Whats the lowest angle difference allowed in which there must be no vehicle for automatic high beams to turn on?
+c_lightAutoHighDetectionDuration = 60  -- Time in ticks before high beams turn on after not detecting a car anymore.
+c_lightAutoHighMinSpeed          = 8.8 -- Minimum speed for the automatic highbeam to activate. (32kmh as per accordance to US law)
+c_lightAutoHighMaxFog            = .18 -- Maximum fog allowed before high beams turn off
+c_lightAutoHighMaxRain           = .15  -- Maximum rain allowed before high beams turn off
+
+c_lightAutoLowMinFog             = .4 -- Minimum fog for low beams to turn on, so you're more visible
+c_lightAutoLowMinRain            = .3 -- Minimum rain for low beams to turn on
