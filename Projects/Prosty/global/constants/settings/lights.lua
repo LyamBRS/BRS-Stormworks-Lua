b1e87837355ba538b21bfa90788451bf25d68d0d
LYAM <cous5830@gmail.com>
2026-04-05 00:52:49 +0000

    Added minimum speed maintenance duration for high beams

--- Projects/Prosty/global/constants/settings/lights.lua
+++ Projects/Prosty/global/constants/settings/lights.lua
@@ -8,6 +8,7 @@ c_clockDayTime            = .32    -- Time where the low beams should turn off
 
 c_lightAutoHighDetectionFOV      = .15 -- Compared to the front of the car, Whats the lowest angle difference allowed in which there must be no vehicle for automatic high beams to turn on?
 c_lightAutoHighDetectionDuration = 60  -- Time in ticks before high beams turn on after not detecting a car anymore.
+c_lightAutoHighSpeedDuration     = 40  -- Time in ticks the car has to be moving above the minimum speed for the high beams to turn on
 c_lightAutoHighMinSpeed          = 8.8 -- Minimum speed for the automatic highbeam to activate. (32kmh as per accordance to US law)
 c_lightAutoHighMaxFog            = .18 -- Maximum fog allowed before high beams turn off
 c_lightAutoHighMaxRain           = .15  -- Maximum rain allowed before high beams turn off
